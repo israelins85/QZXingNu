054bc3064816b43fce7a97641e6a33df462b4ea6
Israel Lins Albuquerque <i.lins85@icloud.com>
2025-11-21 15:40:56 +0100

    missing include for Qt 6.10;

--- qzxingnufilter.h
+++ qzxingnufilter.h
@@ -1,19 +1,18 @@
 #pragma once
 
+#include <QElapsedTimer>
 #include <QImageCapture>
 #include <QMediaCaptureSession>
 #include <QThreadPool>
 #include <QVideoSink>
 #include <qzxingnu.h>
 
-class QZXingNuFilter : public QObject
-{
+class QZXingNuFilter : public QObject {
     Q_OBJECT
     Q_PROPERTY(bool running READ running WRITE setRunning NOTIFY runningChanged)
     Q_PROPERTY(QZXingNu* qzxingNu READ qzxingNu WRITE setQzxingNu NOTIFY qzxingNuChanged)
     Q_PROPERTY(QVideoSink* videoSink READ videoSink WRITE setVideoSink NOTIFY videoSinkChanged)
-    Q_PROPERTY(int intervalDecode READ intervalDecode WRITE setIntervalDecode NOTIFY
-                   intervalDecodeChanged FINAL)
+    Q_PROPERTY(int intervalDecode READ intervalDecode WRITE setIntervalDecode NOTIFY intervalDecodeChanged FINAL)
 
     QZXingNu*     m_qzxingNu  = nullptr;
     QVideoSink*   m_videoSink = nullptr;
@@ -25,8 +24,8 @@ class QZXingNuFilter : public QObject
 public:
     QZXingNuFilter(QObject* parent = nullptr);
 
-    QZXingNu*            qzxingNu() const;
-    QVideoSink*          videoSink() const;
+    QZXingNu*   qzxingNu() const;
+    QVideoSink* videoSink() const;
 
     bool running() const;
     void setRunning(bool newRunning);
